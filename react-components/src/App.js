d842060db1564f61d86d162be7acf1930010a5fe
GJSayers <gemma.sayers1@btinternet.com>
2022-04-13 13:48:26 +0000

    Add class and functional click handlers

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import FunctionalGreeting from "./components/FunctionalGreeting";
-import StatefulGreeting from "./components/StatefulGreeting";
-import HooksCounter from "./components/HooksCounter";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting name="John" />
-      {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
+      <EventsClass />
+      <EventsFunctional />
     </div>
   );
 }
